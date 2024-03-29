c9ab304cae0ce24b7b890a4c7e2d513d4b7f284c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-03-29 17:12:03 +0100

    Initial commit

---
+++ src/main/java/de/kaleidox/netzteil/NetzteilApplication.java
@@ -0,0 +1,11 @@
+package de.kaleidox.netzteil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetzteilApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NetzteilApplication.class, args);
+	}
+}
